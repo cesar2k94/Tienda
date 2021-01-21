a30a3fa369a9f780889c7e149f3e3598e55ef8ab
Cesar2k94 <juliomederosarias@gmail.com>
2021-01-21 19:59:45 -0300

    Panel de administración

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -4,9 +4,12 @@ from django.db import models
 
 class Clientes( models.Model):
     nombre=models.CharField(max_length=30)
-    direccion=models.CharField(max_length=50)
-    email=models.EmailField()
+    direccion=models.CharField(max_length=50, verbose_name="La dirección")
+    email=models.EmailField(blank=True, null=True)
     tfno=models.CharField(max_length=7)
+
+    def __str__(self):
+        return self.nombre
 
 class Articulos(models.Model):
     nombre=models.CharField(max_length=30)
@@ -14,7 +17,7 @@ class Articulos(models.Model):
     precio=models.IntegerField()
 
     def __str__(self):
-        return 'El nombre es %s, la seccion es %s y el precio es %s' %(self.nombre, self.seccion, self.precio)
+        return 'El nombre es %s, la sección es %s y el precio es %s' %(self.nombre, self.seccion, self.precio)
 
 class Pedidos (models.Model):
     numero=models.IntegerField()
